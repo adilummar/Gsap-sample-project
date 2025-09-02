24d19507fb6e27b5a4978e362860e8b4f2a84cae
adil-accolades <adil.accolades@gmail.com>
2025-09-02 12:39:59 +0530

    added glass content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import {ScrollTrigger, SplitText} from "gsap/all";
+import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktiles from "./components/Cocktiles";
 
 gsap.registerPlugin(ScrollTrigger, ScrollTrigger);
 function App() {
-
-    return (
-        <div className={"flex-center h-[100vh]"}>
-           <h1 >this is a text</h1>
-            <h2>this is another text</h2>
-        </div>
-    )
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktiles />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Cocktiles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cocktiles = () => {
+  return (
+    <section id='cocktiles' className="noisy">
+      <img src='/img/cocktail-left-leaf.png' alt='l-leaf' id='c-left-leaf' />
+      <img src='/img/cocktail-right-leaf.png' alt='r-leaf' id='c-right-leaf' />
+    </section>
+  )
+}
+
+export default Cocktiles
