09a01cd061d027566f1c4b288d00709ace1fe04c
adil-accolades <adil.accolades@gmail.com>
2025-09-08 16:59:28 +0530

    cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Cocktiles from "./components/Cocktiles";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, ScrollTrigger);
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Cocktiles />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 }
